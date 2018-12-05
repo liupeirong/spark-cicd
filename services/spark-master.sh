523cce64f69006f4f37b8551ac9d6d98b2547994
liupeirong <liupeirong@gmail.com>
2018-12-04 19:24:29 -0800

    upgrade from spark 2.3 to 2.4

--- services/spark-master.sh
+++ services/spark-master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark-2.3.1-bin-hadoop2.6/conf/spark-defaults.conf
+echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark/conf/spark-defaults.conf
 
-exec /opt/spark-2.3.1-bin-hadoop2.6/bin/spark-class org.apache.spark.deploy.master.Master --ip $HOSTNAME --port 7077 --webui-port 8080
+exec /opt/spark/bin/spark-class org.apache.spark.deploy.master.Master --ip $HOSTNAME --port 7077 --webui-port 8080
 
